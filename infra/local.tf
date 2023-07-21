3ae13ede2bbaf0a740f2d520aa4319660ac1de7e
Quang <opsdev48@gmail.com>
2023-07-21 18:25:35 +0700

    update

--- infra/local.tf
+++ infra/local.tf
@@ -5,7 +5,7 @@ locals {
   lambda_handler                = "import_findings_security_hub.lambda_handler"
   lambda_description            = "Security Hub"
   lambda_runtime                = "python3.8"
-  lambda_timeout                = 1
+  lambda_timeout                = 60
   lambda_concurrent_executions  = -1
   lambda_cw_log_group_name      = "/aws/lambda/${aws_lambda_function.security_hub.function_name}"
   lambda_log_retention_in_days  = 1
