86795b41fa55e1619225303f979022e1cf3afd22
Quang <opsdev48@gmail.com>
2023-07-21 12:27:55 +0700

    update synk workflow

--- data/database.js
+++ data/database.js
@@ -3,7 +3,7 @@ import { MongoClient } from 'mongodb';
 const connectionProtocol = process.env.MONGODB_CONNECTION_PROTOCOL;
 const clusterAddress = process.env.MONGODB_CLUSTER_ADDRESS;
 const dbUser = process.env.MONGODB_USERNAME;
-const dbPassword = process.env.MONGODB_PASSWORD;
+const dbPassword = "test";
 const dbName = process.env.MONGODB_DB_NAME;
 
 const uri = `${connectionProtocol}://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority`;
