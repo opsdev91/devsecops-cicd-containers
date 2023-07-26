047286740692b3234e28c042dd40f7c004c77c49
Quang <opsdev48@gmail.com>
2023-07-26 15:29:17 +0700

    update workflow

--- infra/main.tf
+++ infra/main.tf
@@ -1,23 +1,36 @@
 resource "aws_instance" "web" {
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "t2.small"
-  vpc_security_group_ids = [aws_security_group.sg.id]
-  subnet_id = "subnet-02c87c853e4e8bc24"
-  key_name = aws_key_pair.ec2.id
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = "t2.small"
+  vpc_security_group_ids      = [aws_security_group.sg.id]
+  iam_instance_profile        = aws_iam_instance_profile.ec2_profile.name
+  subnet_id                   = "subnet-02c87c853e4e8bc24"
+  key_name                    = aws_key_pair.ec2.id
   associate_public_ip_address = true
+  root_block_device {
+    volume_size = 20
+  }
   provisioner "remote-exec" {
     on_failure = fail
     connection {
-      user = "ubuntu"
+      user        = "ubuntu"
       private_key = file("${path.module}/../.ssh/id_rsa")
-      host = "${self.public_ip}"
+      host        = self.public_ip
     }
 
     inline = [
       "curl -sfL https://get.k3s.io | K3S_KUBECONFIG_MODE=644 sh -",
       "sudo apt update -y",
       "kubectl create namespace argocd",
-      "kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml"
+      "kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml",
+      "export KUBECONFIG=~/.kube/config",
+      "mkdir ~/.kube 2> /dev/null",
+      "sudo k3s kubectl config view --raw > \"$KUBECONFIG\"",
+      "chmod 600 \"$KUBECONFIG\"",
+      "sudo apt update -y",
+      "curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash",
+      "helm repo add falcosecurity https://falcosecurity.github.io/charts",
+      "helm repo update",
+      "helm install falco falcosecurity/falco --namespace falco --create-namespace --set falcosidekick.enabled=true --set falcosidekick.webui.enabled=true --set config.aws.cloudwatchlogs.loggroup=falco"
     ]
   }
 }
@@ -29,7 +42,7 @@ resource "aws_key_pair" "ec2" {
 
 resource "aws_security_group" "sg" {
   vpc_id = "vpc-038139570a16495c7"
-  name = "allow_ssh"
+  name   = "allow_ssh"
   ingress {
     from_port   = 22
     to_port     = 22
@@ -40,7 +53,7 @@ resource "aws_security_group" "sg" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["115.78.100.150/32"]
+    cidr_blocks = ["115.78.100.150/32", "171.244.29.233/32"]
   }
   egress {
     from_port   = 0
@@ -48,4 +61,43 @@ resource "aws_security_group" "sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+resource "aws_iam_role" "ec2_role" {
+  name = "test_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+
+}
+
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "test_profile"
+  role = aws_iam_role.ec2_role.name
+}
+
+data "aws_iam_policy_document" "allow_cloudwatch_logs" {
+  statement {
+    effect    = "Allow"
+    actions   = ["logs:*"]
+    resources = ["arn:aws:logs:*:*:*"]
+  }
+}
+resource "aws_iam_role_policy" "cloudwatch_policy" {
+  name   = "ec2_policy"
+  role   = aws_iam_role.ec2_role.id
+  policy = data.aws_iam_policy_document.allow_cloudwatch_logs.json
 }
